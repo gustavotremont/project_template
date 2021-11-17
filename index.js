4a501f629aac6ef18ec78da7e3ea98cf24dbe619
gustavotremont <gustavotremontsr@gmail.com>
2021-11-17 23:59:42 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-/****************** Dependencies ******************/
+/****************** NPM Dependencies ******************/
 const express = require('express')
+
+/****************** Project Dependencies ******************/
+const errors = require('./middlewares/errors');
 
 /****************** Enable Express ******************/
 const app = express()
@@ -14,15 +17,13 @@ app.use(express.urlencoded()); //Habilita la lectura del body por metodo post
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-/****************** Paths ******************/
+/****************** Routes ******************/
 app.get('/', (req, res) => {
     res.render('helloWorld')
 })
 
 //Capture All 404 errors
-app.use( (req,res,next) => {
-    res.status(404).render('error');
-});
+app.use( errors.error404);
 
 /****************** Actice Server ******************/
 app.listen(port, () => {
