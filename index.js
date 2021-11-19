991e77108ac618729dc5a81e865407085a53f1ac
Gustavo <gustavotremontsr@gmail.com>
2021-11-19 14:20:54 +0100

    add morgan and routes

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-/****************** NPM Dependencies ******************/
+/****************** Nodejs Dependencies ******************/
 const express = require('express')
+const logger = require('morgan');
 
 /****************** Project Dependencies ******************/
 const errors = require('./middlewares/errors');
+
+/****************** Import routes ******************/
+const indexRoutes = require('./routes/index')
 
 /****************** Enable Express ******************/
 const app = express()
 const port = 3000
 
-/****************** Express Options ******************/
+/****************** Express Settings ******************/
 app.use(express.json()); //Para habilitar envio de JSON al servidor
 app.use(express.static('public')); //Habilitar los archivos para que sean estaticos
-app.use(express.urlencoded()); //Habilita la lectura del body por metodo post
+app.use(express.urlencoded( { extended: false } )); //Habilita la lectura del body por metodo post
+app.use(logger('dev')) // habilitar Morgan con preset dev
 
 /****************** Enable Pug ******************/
 app.set('view engine', 'pug');
 app.set('views','./views');
 
 /****************** Routes ******************/
-app.get('/', (req, res) => {
-    res.render('helloWorld')
-})
+app.use('/', indexRoutes);
 
 //Capture All 404 errors
 app.use( errors.error404);
